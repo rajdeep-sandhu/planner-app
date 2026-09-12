65629d98e192a35115a2c926266612e0a8c0daa0
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-09-12 21:09:56 +0000

    add get_element_start()

--- src/planner/autoplan.py
+++ src/planner/autoplan.py
@@ -1,6 +1,16 @@
 # autoplan.py
 
+from datetime import date, datetime
+
+
 class Autoplan:
     """
     Autoplans Production jobs from planning data.
     """
+
+    def get_element_start(self, plan_date: date, sku: str, job_number: str) -> datetime:
+        """
+        Get the start time of the job element defined by job_number, sku and plan_date.
+        """
+
+        raise NotImplementedError
