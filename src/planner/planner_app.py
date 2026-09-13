617f47c7ec28d467f5502c56722e02fa72cee388
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-09-13 11:04:40 +0000

    rename planner.py to planner_app.py

--- src/planner/planner_app.py
+++ src/planner/planner_app.py
@@ -1,19 +1,20 @@
 import marimo
 
 __generated_with = "0.24.2"
-app = marimo.App(width="full", app_title="Planner")
+app = marimo.App(width="full", app_title="Planner App")
 
-with app.setup:
+
+@app.cell
+def _():
     import marimo as mo
 
 
 @app.cell(hide_code=True)
-def _():
+def _(mo):
     mo.md(r"""
     # Planner App
     """)
     return
-
 
 if __name__ == "__main__":
     app.run()
