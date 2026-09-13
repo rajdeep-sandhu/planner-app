ef84ecf0792479dbd9f56b29cec23d9362147ef6
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-09-13 06:39:31 +0000

    add _load_lists_from_excel()

--- src/planner/config_lists.py
+++ src/planner/config_lists.py
@@ -1,13 +1,33 @@
 # config_lists.py
 from pathlib import Path
 
+import polars as pl
+
 
 class ConfigLists:
     """Set up configuration lists."""
 
     def __init__(self):
         """ConfigLists constructor."""
-        self.config_lists: dict = {}
         self.gammon_plan_workbook: Path = (
             Path(__file__).parent / "dev_data" / "Gammon_Plan_XL_V3_DEMO_WIP.xlsm"
         )
+        self.config_lists: dict[str, list] = self._load_lists_from_excel()
+
+    def _load_lists_from_excel(self) -> dict[str, list]:
+        """
+        Load lists from List worksheet of gammon_plan_workbook as DataFrame.
+        
+        Return:
+        lists: dict of config lists.
+        """
+        lists_df: pl.DataFrame = pl.read_excel(
+            self.gammon_plan_workbook, sheet_name="List"
+        )
+
+        lists: dict[str, list] = {
+            column: lists_df[column].drop_nulls().to_list()
+            for column in lists_df.columns
+        }
+
+        return lists
