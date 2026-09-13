902ec90d01b02ea8fc0634152c2fd9e5ec7145cd
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-09-13 10:42:37 +0000

    add class constructor

--- src/planner/db/duckdb_factory.py
+++ src/planner/db/duckdb_factory.py
@@ -1,6 +1,13 @@
 # duckdb_factory.py
 
+
 class DuckDBFactory:
     """
     DuckDB Factory.
     """
+
+    def __init__(self, database: str = ":memory") -> None:
+        """
+        DuckDBFactory Constructor.
+        """
+        self._database: str = database
