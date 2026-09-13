b1e94d9e1ec6148672b2378fc301198121f56bdc
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-09-13 06:18:07 +0000

    add ConfigLists constructor

--- src/planner/config_lists.py
+++ src/planner/config_lists.py
@@ -1,4 +1,13 @@
 # config_lists.py
+from pathlib import Path
+
 
 class ConfigLists:
     """Set up configuration lists."""
+
+    def __init__(self):
+        """ConfigLists constructor."""
+        self.config_lists: dict = {}
+        self.gammon_plan_workbook: Path = (
+            Path(__file__).parent / "dev_data" / "Gammon_Plan_XL_V3_DEMO_WIP.xlsm"
+        )
