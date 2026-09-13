f65d2987a6e232c087b4e39c64c0f3cbd3cb6285
Rajdeep Sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2026-09-13 13:54:37 +0000

    add create_connection()

--- src/planner/db/duckdb_factory.py
+++ src/planner/db/duckdb_factory.py
@@ -1,4 +1,5 @@
 # duckdb_factory.py
+import duckdb
 
 
 class DuckDBFactory:
@@ -6,8 +7,10 @@ class DuckDBFactory:
     DuckDB Factory.
     """
 
-    def __init__(self, database: str = ":memory") -> None:
-        """
-        DuckDBFactory Constructor.
-        """
+    def __init__(self, database: str = ":memory:") -> None:
+        """DuckDBFactory Constructor."""
         self._database: str = database
+
+    def create_connection(self) -> duckdb.DuckDBPyConnection:
+        """Create a DuckDB connection."""
+        return duckdb.connect(self._database)
